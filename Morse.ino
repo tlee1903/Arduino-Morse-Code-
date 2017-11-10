a9679d3f59572de7cc6e42cef9579fbb3d36d486
tlee1903 <tlee1903@mpsedu.org>
2017-11-10 10:04:56 -0600

    Update Morse.ino

--- Morse.ino
+++ Morse.ino
@@ -3,7 +3,7 @@
   Turns on an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  it is attached to digital pin 13, on MKRdash on pin 6. LED_BUILTIN is set to
   the correct LED pin independent of which board is used.
   If you want to know what pin the on-board LED is connected to on your Arduino model, check
   the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
@@ -29,139 +29,144 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  #define dash 1000
+  #define blinc 200
+  #define pause 3000
+  
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level) G START
-  delay(1000);                       // wait for a second
+  delay(dash);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(dash);                       // wait for a second
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);                        
+  delay(pause);                        
 
   digitalWrite(LED_BUILTIN, HIGH);   // I 
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);          
+  delay(dash);          
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);                        
+  delay(pause);                        
 
   digitalWrite(LED_BUILTIN, HIGH);    // T 
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(3000);                        
+  delay(pause);                        
 
   digitalWrite(LED_BUILTIN, HIGH);   // G 
-  delay(1000);                       
+  delay(dash);                       
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);                       
+  delay(dash);                       
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);                        
+  delay(pause);                        
 
   digitalWrite(LED_BUILTIN, HIGH);    // U
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);                  
+  delay(dash);                  
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);       
+  delay(dash);       
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(3000);           
+  delay(pause);           
 
   digitalWrite(LED_BUILTIN, HIGH);  // D 
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(1000);     
+  delay(dash);     
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);  
+  delay(dash);  
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);                      
+  delay(pause);                      
 
   digitalWrite(LED_BUILTIN, HIGH);  // S 
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);  
+  delay(dash);  
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);  
+  delay(dash);  
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);  
+  delay(pause);  
 
   digitalWrite(LED_BUILTIN, HIGH);   // K
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(1000);     
+  delay(dash);     
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);  
+  delay(dash);  
   digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(3000);     
+  delay(pause);     
 
   digitalWrite(LED_BUILTIN, HIGH);  // R
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);  
+  delay(dash);  
   digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(1000);    
+  delay(dash);    
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000); 
+  delay(pause); 
 
   digitalWrite(LED_BUILTIN, HIGH);  // U
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, HIGH);  
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(3000);    
+  delay(pause);    
 
   digitalWrite(LED_BUILTIN, HIGH);    // B
-  delay(1000);
+  delay(dash);
   digitalWrite(LED_BUILTIN, LOW);  
-  delay(1000);  
+  delay(dash);  
    digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);
+  delay(dash);
    digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);
+  delay(dash);
    digitalWrite(LED_BUILTIN, HIGH);
-  delay(200);
+  delay(blinc);
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);  
+  delay(pause);  
   
-  // GIT GUD SKRUB
+  // GIT GUD SKRUB               
+}
